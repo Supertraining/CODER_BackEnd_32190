0e6222f8709e8551031a3ba5425424bb9c33b212
Matías Maranga <marangamatias@gmail.com>
2022-11-02 02:39:52 -0300

    correccionesPostAfter

--- DesafioNº2/manejoDeArchivos.js
+++ DesafioNº2/manejoDeArchivos.js
@@ -1,66 +1,77 @@
 const fs = require('fs');
 
 class contenedor {
-	constructor(name) {
-		this.name = name;
-		this.id = 1;
+	constructor(path) {
+		this.path = path;
 	}
 
 	async save(obj) {
-		const file = `./${this.name}`;
+		let data = null;
+		let newProducto = null;
+		let id = 1;
 
 		try {
-			if (fs.existsSync(file)) {
-				const producto = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-				const newProducto = JSON.parse(producto);
-				newProducto.push({ ...obj, id: newProducto.length + 1 });
-				await fs.promises.writeFile(`./${this.name}`, JSON.stringify(newProducto));
+			data = await fs.promises.readFile(`${this.path}`, 'utf-8');
+		} catch (err) {
+			data = [];
+		}
 
-				const index = newProducto.length - 1;
-				return newProducto[index].id;
-			} else {
-				await fs.promises.writeFile(
-					`./${this.name}`,
-					JSON.stringify([{ ...obj, id: this.id }])
-				);
-				return this.id;
-			}
+		if (data.length === 0) {
+			await fs.promises.writeFile(`${this.path}`, JSON.stringify([{ ...obj, id: id }]));
+			return id;
+		} else {
+			newProducto = JSON.parse(data);
+			newProducto.push({ ...obj, id: newProducto.length + 1 });
+		}
+		try {
+			await fs.promises.writeFile(`${this.path}`, JSON.stringify(newProducto));
 		} catch (err) {
 			throw new Error('Ocurrio un error' + err);
 		}
+		const index = newProducto.length - 1;
+		return newProducto[index].id;
 	}
 
 	async getById(n) {
+		let data = null;
 		try {
-			const productos = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-			const newProductos = JSON.parse(productos);
-			const resultado = newProductos.find((e) => e.id === n);
-			if (resultado) {
-				return resultado;
-			} else {
-				return null;
-			}
+			data = await fs.promises.readFile(`${this.path}`, 'utf-8');
 		} catch (err) {
 			throw new Error('Ocurrio un error');
+		}
+		const newData = JSON.parse(data);
+		const resultado = newData.find((e) => e.id === n);
+		if (resultado) {
+			return resultado;
+		} else {
+			return null;
 		}
 	}
 
 	async getAll() {
+		let data = null;
 		try {
-			const productos = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-			const newProductos = JSON.parse(productos);
-			return newProductos;
+			data = await fs.promises.readFile(`${this.path}`, 'utf-8');
+			return data;
 		} catch (err) {
-			throw new Error('Ocurrio un error');
+			return (data = []);
 		}
 	}
 
 	async deleteById(n) {
+		let data = null;
 		try {
-			const productos = await fs.promises.readFile(`./${this.name}`, 'utf-8');
-			const newProductos = JSON.parse(productos);
-			const resultado = newProductos.filter((e) => e.id !== n);
-			await fs.promises.writeFile(`./${this.name}`, JSON.stringify(resultado));
+			data = await fs.promises.readFile(`${this.path}`, 'utf-8');
+		} catch (err) {
+			throw new Error('Ocurrio un error');
+		}
+		const newProductos = JSON.parse(data);
+		if (n < 1 || n > newProductos.length) {
+			throw new Error('No existe el producto');
+		}
+		const resultado = newProductos.filter((e) => e.id !== n);
+		try {
+			await fs.promises.writeFile(`${this.path}`, JSON.stringify(resultado));
 		} catch (err) {
 			throw new Error('Ocurrio un error');
 		}
@@ -68,21 +79,21 @@ class contenedor {
 
 	async deleteAll() {
 		try {
-			await fs.promises.writeFile(`./${this.name}`, '');
+			await fs.promises.writeFile(`${this.path}`, '');
 		} catch (err) {
 			throw new Error('Ocurrio un error');
 		}
 	}
 }
 
-const data = new contenedor('productos.txt');
+const data = new contenedor('./productos.txt');
 
 data.save({
-	title: 'acelga',
-	price: 290,
-	thumbnail: 'https://www.gastronomiavasca.net/uploads/image/file/3309/w700_acelgas.jpg',
+	title: 'Sandias',
+	price: 1500,
+	thumbnail: 'https://www.cucinare.tv/wp-content/uploads/2019/04/Sandias.jpg',
 });
-// data.getById(3).then(value => console.log(value))
+// data.getById(4).then(value => console.log(value))
 // data.getAll().then(value => console.log(value))
-// data.deleteById(3)
+// data.deleteById(2)
 // data.deleteAll()
